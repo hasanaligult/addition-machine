9736b30efb26bc51a57c0790bbc3209b43d9f598
Hasan Ali Gültekin <36507521+hasanaligult@users.noreply.github.com>
2020-03-12 13:08:39 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace HASANALIGULTEKIN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEkran = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEkran
+            // 
+            this.txtEkran.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtEkran.Location = new System.Drawing.Point(0, 0);
+            this.txtEkran.Margin = new System.Windows.Forms.Padding(5);
+            this.txtEkran.Name = "txtEkran";
+            this.txtEkran.ReadOnly = true;
+            this.txtEkran.Size = new System.Drawing.Size(408, 26);
+            this.txtEkran.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(45, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(166, 76);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(285, 76);
+            this.button3.Margin = new System.Windows.Forms.Padding(5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(45, 146);
+            this.button4.Margin = new System.Windows.Forms.Padding(5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(166, 146);
+            this.button5.Margin = new System.Windows.Forms.Padding(5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Location = new System.Drawing.Point(285, 146);
+            this.button6.Margin = new System.Windows.Forms.Padding(5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Location = new System.Drawing.Point(45, 214);
+            this.button7.Margin = new System.Windows.Forms.Padding(5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(74, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button8
+            // 
+            this.button8.AutoSize = true;
+            this.button8.Location = new System.Drawing.Point(166, 214);
+            this.button8.Margin = new System.Windows.Forms.Padding(5);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(74, 40);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSize = true;
+            this.button9.Location = new System.Drawing.Point(285, 214);
+            this.button9.Margin = new System.Windows.Forms.Padding(5);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(74, 40);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button10
+            // 
+            this.button10.AutoSize = true;
+            this.button10.Location = new System.Drawing.Point(45, 281);
+            this.button10.Margin = new System.Windows.Forms.Padding(5);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(74, 40);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button11
+            // 
+            this.button11.AutoSize = true;
+            this.button11.Location = new System.Drawing.Point(166, 281);
+            this.button11.Margin = new System.Windows.Forms.Padding(5);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(74, 40);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "+";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.RakamDugme_Click);
+            // 
+            // button12
+            // 
+            this.button12.AutoSize = true;
+            this.button12.Location = new System.Drawing.Point(285, 281);
+            this.button12.Margin = new System.Windows.Forms.Padding(5);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(74, 40);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "=";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.IslemDugme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(408, 385);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtEkran);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEkran;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HASANALIGULTEKIN
+{
+    public partial class Form1 : Form
+    {
+       
+         private Rectangle txtEkranOrginalRect;
+         private Rectangle button1OriginalRect;
+         private Rectangle button2OriginalRect;
+         private Rectangle button3OriginalRect;
+         private Rectangle button4OriginalRect;
+         private Rectangle button5OriginalRect;
+         private Rectangle button6OriginalRect;
+         private Rectangle button7OriginalRect;
+         private Rectangle button8OriginalRect;
+         private Rectangle button9OriginalRect;
+         private Rectangle button10OriginalRect;
+         private Rectangle button11OriginalRect;
+         private Rectangle button12OriginalRect;
+
+         private Size formOriginalSize;
+
+        Button[] Dugmeler;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+
+        private void RakamDugme_Click(object sender, EventArgs e)
+        {
+            Button rakamDugme;
+            rakamDugme = (Button)sender;
+            txtEkran.Text += rakamDugme.Text;           
+        }
+
+        private void IslemDugme_Click(object sender, EventArgs e)
+        {
+            char[] ayiraclar= new char[] {'+'};
+            string[] strSayilar;
+            strSayilar = txtEkran.Text.Split(ayiraclar);
+
+            int toplam = 0;
+            foreach(string strSayi in strSayilar)
+            {
+                toplam += Convert.ToInt32(strSayi);
+            }
+            txtEkran.Text = toplam.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dugmeler = new Button[12] {button1,button2,button3,button4,button5,button6,button7,button8,
+            button9,button10,button11,button12};
+
+            int bos = 5;
+            int penGenislik, penYukseklik;
+            int btnGenislik, btnYukseklik;
+                        
+            txtEkran.TextAlign = HorizontalAlignment.Right;
+            
+            penGenislik = this.ClientRectangle.Width;
+            penYukseklik = this.ClientRectangle.Height;
+
+            btnGenislik = (int)(penGenislik - 5 * bos) / 3;
+            btnYukseklik = (int)(penYukseklik - txtEkran.Height - 6 * bos) / 4;
+
+            txtEkran.Left = bos;
+            txtEkran.Top = bos;
+            txtEkran.Width = penGenislik - 2 * bos;
+            for (int i = 0; i < 12; i++)
+            {
+                Dugmeler[i].Height = btnYukseklik;
+                Dugmeler[i].Width = btnGenislik;
+                Dugmeler[i].Top = txtEkran.Bottom + bos + (int)(i / 3) * (btnYukseklik + bos);
+                Dugmeler[i].Left = txtEkran.Left + bos + (int)(i % 3) * (btnGenislik + bos);              
+            }
+
+            formOriginalSize = this.Size;
+
+            txtEkranOrginalRect = new Rectangle(txtEkran.Location.X, txtEkran.Location.Y, txtEkran.Width, txtEkran.Height);
+            button1OriginalRect = new Rectangle(button1.Location.X, button1.Location.Y, button1.Width, button1.Height);
+            button2OriginalRect = new Rectangle(button2.Location.X, button2.Location.Y, button2.Width, button2.Height);
+            button3OriginalRect = new Rectangle(button3.Location.X, button3.Location.Y, button3.Width, button3.Height);
+            button4OriginalRect = new Rectangle(button4.Location.X, button4.Location.Y, button4.Width, button4.Height);
+            button5OriginalRect = new Rectangle(button5.Location.X, button5.Location.Y, button5.Width, button5.Height);
+            button6OriginalRect = new Rectangle(button6.Location.X, button6.Location.Y, button6.Width, button6.Height);
+            button7OriginalRect = new Rectangle(button7.Location.X, button7.Location.Y, button7.Width, button7.Height);
+            button8OriginalRect = new Rectangle(button8.Location.X, button8.Location.Y, button8.Width, button8.Height);
+            button9OriginalRect = new Rectangle(button9.Location.X, button9.Location.Y, button9.Width, button9.Height);
+            button10OriginalRect = new Rectangle(button10.Location.X, button10.Location.Y, button10.Width, button10.Height);
+            button11OriginalRect = new Rectangle(button11.Location.X, button11.Location.Y, button11.Width, button11.Height);
+            button12OriginalRect = new Rectangle(button12.Location.X, button12.Location.Y, button12.Width, button12.Height);
+        }
+         
+        private void resizeChildrenControls()
+        {
+             resizeControl(txtEkranOrginalRect, txtEkran);
+             resizeControl(button1OriginalRect, button1);
+             resizeControl(button2OriginalRect, button2);
+             resizeControl(button3OriginalRect, button3);
+             resizeControl(button4OriginalRect, button4);
+             resizeControl(button5OriginalRect, button5);
+             resizeControl(button6OriginalRect, button6);
+             resizeControl(button7OriginalRect, button7);
+             resizeControl(button8OriginalRect, button8);
+             resizeControl(button9OriginalRect, button9);
+             resizeControl(button10OriginalRect, button10);
+             resizeControl(button11OriginalRect, button11);
+             resizeControl(button12OriginalRect, button12);
+        }
+
+        private void resizeControl(Rectangle originalControlRect, Control control)
+         {
+            float xRatio= (float)(this.Width)/(float)(formOriginalSize.Width);
+            float yRatio= (float)(this.Height)/(float)(formOriginalSize.Height);
+
+            int newX =(int)(originalControlRect.X * xRatio);
+            int newY = (int)(originalControlRect.Y * yRatio);
+            int newWidth = (int)(originalControlRect.Width * xRatio);
+            int newHeight = (int)(originalControlRect.Height * yRatio);
+
+            control.Location = new Point(newX, newY);
+            control.Size = new Size(newWidth, newHeight);
+         }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeChildrenControls();
+        }
+       
+    }
+}
